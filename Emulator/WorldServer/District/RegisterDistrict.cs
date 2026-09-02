516cf3cfeee344337f42eb0aa365718db5feea2a
quette <gsaranov@gmail.com>
2026-09-02 21:43:14 +0300

    fix

--- Emulator/WorldServer/District/RegisterDistrict.cs
+++ Emulator/WorldServer/District/RegisterDistrict.cs
@@ -26,52 +26,85 @@ namespace WorldServer.Districts
             }
 
             district.tcp = tcpClient;
-            Log.Info("RegisterDistrict", district.ToString() + " tries to register.");
+
+            Log.Info(
+                "RegisterDistrict",
+                district.ToString() + " tries to register."
+            );
+
             if (ID != 0)
             {
-                // District authentication disabled for now.
-                // The older working implementation explicitly disabled
-                // this check because it prevented districts from connecting.
-    
-                district.Id = ID;
-                MyDB.AccountEntry acc = Databases.AccountTable.SingleOrDefault(a => a.Token == token);
-                if (acc.CanHostDistrict == 0 || acc.Index < 1)
-                {
-                    district.tcp.Client.Send(new byte[] { 0x30, 0x30 });
-                    district.tcp.Client.Disconnect(true);
-                    return;
-                }
                 district.Id = ID;
                 district.IP = ip;
                 district.Port = Convert.ToUInt16(port);
+
                 lock (Program.districtsListener.Districts)
                 {
                     uint code = (uint)(district.Type << 24);
                     code += district.Id;
-                    foreach (KeyValuePair<uint, District> dist in Program.districtsListener.Districts)
+
+                    foreach (
+                        KeyValuePair<uint, District> dist
+                        in Program.districtsListener.Districts
+                    )
                     {
-                        if (dist.Value.IP == ip && Program.districtsListener.Districts.ContainsKey(code))
+                        if (
+                            dist.Value.IP == ip &&
+                            Program.districtsListener.Districts.ContainsKey(code)
+                        )
                         {
-                            district.tcp.Client.Send(new byte[] { 0x30, 0x31 });
+                            district.tcp.Client.Send(
+                                new byte[] { 0x30, 0x31 }
+                            );
+
                             Program.districtsListener.Districts.Remove(code);
-                            return;
+                            break;
                         }
-                        else if (dist.Value.IP != ip && Program.districtsListener.Districts.ContainsKey(code))
+                        else if (
+                            dist.Value.IP != ip &&
+                            Program.districtsListener.Districts.ContainsKey(code)
+                        )
                         {
-                            Log.Error("RegisterDistrict", "Fail try of district registration that already exists!");
-                            district.tcp.Client.Send(new byte[] { 0x30, 0x32 });
+                            Log.Error(
+                                "RegisterDistrict",
+                                "Fail try of district registration that already exists!"
+                            );
+
+                            district.tcp.Client.Send(
+                                new byte[] { 0x30, 0x32 }
+                            );
+
                             return;
                         }
                     }
-                    Program.districtsListener.Districts.Add(code, district);
-                    Program.districtsListener.DistrictsTcp.Add(district.tcp, code);
+
+                    Program.districtsListener.Districts.Add(
+                        code,
+                        district
+                    );
+
+                    Program.districtsListener.DistrictsTcp.Add(
+                        district.tcp,
+                        code
+                    );
                 }
-                Log.Succes("RegisterDistrict", district + " was registered! (" + ip + ":" + port + ")");
-                district.tcp.Client.Send(new byte[] { 0x30, 0x33 });
+
+                Log.Succes(
+                    "RegisterDistrict",
+                    district + " was registered! (" +
+                    ip + ":" + port + ")"
+                );
+
+                district.tcp.Client.Send(
+                    new byte[] { 0x30, 0x33 }
+                );
             }
             else
             {
-                district.tcp.Client.Send(new byte[] { 0x30, 0x34 });
+                district.tcp.Client.Send(
+                    new byte[] { 0x30, 0x34 }
+                );
+
                 district.tcp.Client.Disconnect(true);
                 return;
             }
